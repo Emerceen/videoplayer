36548eddbec64d84918e20d791e6aab2ba760fc8
MarcinDedio <marcindedio@gmail.com>
2017-01-28 11:43:44 +0100

    Create a component Player
    Component with implements OnInit abstract class, and boolean initial method ngOnInit

---
+++ src/app/player/player.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'as-player',
+  templateUrl: 'player.component.html'
+})
+
+export class PlayerComponent implements OnInit {
+
+  ngOnInit(): boolean {
+    return true;
+  }
+}
