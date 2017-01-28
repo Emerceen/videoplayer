20ca23a71bc13d32a3ef1e0f7c81c897f5cabfda
MarcinDedio <marcindedio@gmail.com>
2017-01-28 11:46:20 +0100

    Create a module Player and index file
    Module with declaration, and export PlayerComponent.
    Index file is used to export the classes associated with the component player

---
+++ src/app/player/player.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { PlayerComponent } from './index';
+
+
+@NgModule({
+  exports: [
+    PlayerComponent
+  ],
+  declarations: [
+    PlayerComponent
+  ]
+})
+
+export class PlayerModule { }
