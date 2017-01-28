befb3adf2c5dab41951afce90889f7d44829ec95
MarcinDedio <marcindedio@gmail.com>
2017-01-28 16:12:05 +0100

    Adding service to the main module
    Push provide object with VideoService class and Http depedency to providers array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule, Http } from '@angular/http';
 
 import { APP_PROVIDERS } from './app.providers';
 import { AppComponent } from './app.component';
 import { appRoutingProviders, routing } from './app.routing';
+
+import { VideoService } from './services/video.service';
 
 import { PlayerModule } from './player/player.module';
 
@@ -15,9 +18,20 @@ import { PlayerModule } from './player/player.module';
     imports: [
         BrowserModule,
         PlayerModule,
+        HttpModule,
         routing
     ],
-    providers: [ APP_PROVIDERS, appRoutingProviders ],
+    providers: [
+        APP_PROVIDERS,
+        appRoutingProviders,
+        {
+            provide: VideoService,
+            useFactory: (http) => {
+                return new VideoService(http);
+            },
+            deps: [Http]
+        },
+    ],
     bootstrap: [ AppComponent ]
 })
 export class AppModule {
