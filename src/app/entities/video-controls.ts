e1d4aa6fe1a3198a9ae2b028a7867d70e4e92814
marcin.dedio <marcindedio@gmail.com>
2017-02-21 11:25:36 +0100

    Adding controls property to video entity
    Controls are responsible for storing the state of video.

---
+++ src/app/entities/video-controls.ts
@@ -0,0 +1,7 @@
+export class VideoControls {
+  constructor(
+    public played = false,
+    public stopped = true,
+    public repeated = false
+  ) {}
+}
--- src/app/mock/video-mock.ts
+++ src/app/mock/video-mock.ts
@@ -1,4 +1,4 @@
-import { Video } from '../entities/video';
+import { Video, VideoInterface } from '../entities/video';
 
 
 export class VideoMock {
@@ -6,28 +6,48 @@ export class VideoMock {
     {
       name: 'VIDEO 1',
       url: 'http://testwebapplication.cba.pl/674127941.mp4',
-      type: 'video/mp4'
+      type: 'video/mp4',
+      controls: {
+        played: false,
+        stopped: true,
+        repeated: false
+      }
     },
     {
       name: 'VIDEO 2',
       url: 'http://testwebapplication.cba.pl/675076297.mp4',
-      type: 'video/mp4'
+      type: 'video/mp4',
+      controls: {
+        played: false,
+        stopped: true,
+        repeated: false
+      }
     },
     {
       name: 'VIDEO 3',
       url: 'http://testwebapplication.cba.pl/675076800.mp4',
-      type: 'video/mp4'
+      type: 'video/mp4',
+      controls: {
+        played: false,
+        stopped: true,
+        repeated: false
+      }
     },
     {
       name: 'VIDEO 4',
       url: 'http://testwebapplication.cba.pl/675077654.mp4',
-      type: 'video/mp4'
+      type: 'video/mp4',
+      controls: {
+        played: false,
+        stopped: true,
+        repeated: false
+      }
     }
   ];
 }
 
 export class VideoMockPascalCase {
-  public Videos = [
+  public Videos: Array<VideoInterface> = [
     {
       Name: 'VIDEO 2',
       Url: 'http://testwebapplication.cba.pl/675076297.mp4',
