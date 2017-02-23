4890c3bc9fc903d881d294116a014083eea49e63
marcin.dedio <marcindedio@gmail.com>
2017-02-23 10:42:59 +0100

    Implementation stylesheet in player-settings.component

--- src/app/player-settings/player-settings.component.ts
+++ src/app/player-settings/player-settings.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'as-player-settings',
-  templateUrl: 'player-settings.component.html'
+  templateUrl: 'player-settings.component.html',
+  styleUrls: ['player-settings.css']
 })
 
 
