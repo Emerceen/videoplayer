6a1a10795d85dfa598a1307489501c481787b444
marcin.dedio <marcindedio@gmail.com>
2017-04-10 13:22:44 +0200

    Adding component

---
+++ src/app/player-progress-bar/player-progress-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'as-player-progress-bar',
+  templateUrl: 'player-progress-bar.component.html'
+})
+
+export class PlayerProgressBarComponent {
+
+}
