73cf536c48e43318b3e85f343bfb9146e2e5fc9a
marcin.dedio <marcindedio@gmail.com>
2017-04-10 16:02:23 +0200

    Adding time duration handler

--- src/app/player-progress-bar/player-progress-bar.component.ts
+++ src/app/player-progress-bar/player-progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
 })
 
 export class PlayerProgressBarComponent {
+  public duration: number = 0;
+  @Input() public set videoElement(element: { nativeElement: HTMLVideoElement }) {
+    if (element.nativeElement) {
+      this._videoElement = element;
+      this.registerTimeUpdate();
+    }
+  };
 
+  public get videoElement(): { nativeElement: HTMLVideoElement } {
+    return this._videoElement;
+  };
+
+  private _videoElement: { nativeElement: HTMLVideoElement };
+
+  registerTimeUpdate(): void {
+    this.videoElement.nativeElement.ontimeupdate = () => { this.timeLog(); };
+  }
+
+  timeLog(): void {
+    this.duration = Math.floor((100 / this.videoElement.nativeElement.duration) * this.videoElement.nativeElement.currentTime);
+  }
 }
