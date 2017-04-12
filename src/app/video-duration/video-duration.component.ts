306fbe5c3f7392f30f737259da6457dd48aa15e8
marcin.dedio <marcindedio@gmail.com>
2017-04-12 13:37:25 +0200

    Adding VideoDurationComponent

---
+++ src/app/video-duration/video-duration.component.ts
@@ -0,0 +1,28 @@
+import { TimeString } from './../time-string/time-string.service';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'as-video-duration',
+  templateUrl: 'video-duration.component.html'
+})
+
+export class VideoDurationComponent {
+  @Input() public set currentTime(value: number) {
+    if (value > 0) {
+      this.currentTimeTimeString = this.timeString.toHHMMSS(value);
+    }
+  };
+  @Input() public set duration(value: number) {
+    if (value > 0) {
+      this.durationTimeString = this.timeString.toHHMMSS(value);
+    }
+  };
+
+  public durationTimeString: string;
+  public currentTimeTimeString: string = '0:00';
+
+  constructor(
+    private timeString: TimeString
+  ) { }
+}
