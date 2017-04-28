5d7568845f22987b98f9ed1609af308c63db85cd
marcin.dedio <marcindedio@gmail.com>
2017-04-28 14:48:52 +0200

    Updating mock

--- src/app/mock/video-mock.spec.ts
+++ src/app/mock/video-mock.spec.ts
@@ -7,24 +7,36 @@ export class VideoMock {
       name: 'VIDEO 1',
       url: 'http://testwebapplication.cba.pl/674127941.mp4',
       type: 'video/mp4',
+      channel: 'channel 1',
+      views: 100,
+      poster: 'poster',
       playedInShuffle: false
     },
     {
       name: 'VIDEO 2',
       url: 'http://testwebapplication.cba.pl/675076297.mp4',
       type: 'video/mp4',
+      channel: 'channel 1',
+      views: 100,
+      poster: 'poster',
       playedInShuffle: false
     },
     {
       name: 'VIDEO 3',
       url: 'http://testwebapplication.cba.pl/675076800.mp4',
       type: 'video/mp4',
+      channel: 'channel 1',
+      views: 100,
+      poster: 'poster',
       playedInShuffle: false
     },
     {
       name: 'VIDEO 4',
       url: 'http://testwebapplication.cba.pl/675077654.mp4',
       type: 'video/mp4',
+      channel: 'channel 1',
+      views: 100,
+      poster: 'poster',
       playedInShuffle: false
     }
   ];
@@ -35,16 +47,25 @@ export class VideoMockPascalCase {
     {
       Name: 'VIDEO 2',
       Url: 'http://testwebapplication.cba.pl/675076297.mp4',
+      Channel: 'channel 1',
+      Views: 100,
+      Poster: 'poster',
       Type: 'video/mp4'
     },
     {
       Name: 'VIDEO 3',
       Url: 'http://testwebapplication.cba.pl/675076800.mp4',
+      Channel: 'channel 1',
+      Views: 100,
+      Poster: 'poster',
       Type: 'video/mp4'
     },
     {
       Name: 'VIDEO 4',
       Url: 'http://testwebapplication.cba.pl/675077654.mp4',
+      Channel: 'channel 1',
+      Views: 100,
+      Poster: 'poster',
       Type: 'video/mp4'
     }
   ];
