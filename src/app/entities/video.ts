8555769e0ab546913fa5e68178211540f33c3663
marcin.dedio <marcindedio@gmail.com>
2017-04-28 11:38:04 +0200

    Modifying mock api data and Video class

--- src/app/entities/video.ts
+++ src/app/entities/video.ts
@@ -4,17 +4,23 @@ export interface VideoInterface {
   Name: string;
   Url: string;
   Type: string;
+  Channel: string;
+  Poster: string;
+  Views: number;
 }
 
 export class Video {
   static fromData(data: VideoInterface): Video {
-    return new Video(data.Name, data.Url, data.Type);
+    return new Video(data.Name, data.Url, data.Type, data.Channel, data.Poster, data.Views);
   }
 
   constructor(
     public name: string,
     public url: string,
     public type: string,
+    public channel: string,
+    public poster: string,
+    public views: number,
     public playedInShuffle: boolean = false,
     public safeUrl?: SafeUrl
   ) { }
