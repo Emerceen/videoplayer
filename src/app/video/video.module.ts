fe981afbe46613f2d1bab9e73edfbe6968b06d6b
marcin.dedio <marcindedio@gmail.com>
2017-04-28 13:28:33 +0200

    Import SharedModule in to Video module

--- src/app/video/video.module.ts
+++ src/app/video/video.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { VideoComponent } from './video.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     VideoComponent
+  ],
+  imports: [
+    SharedModule
   ],
   exports: [
     VideoComponent
