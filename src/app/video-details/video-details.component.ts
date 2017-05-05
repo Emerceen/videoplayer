29248d1c717dcd1ea68d6c75f5df3a9d357d87fe
marcin.dedio <marcindedio@gmail.com>
2017-05-05 13:01:05 +0200

    Implementation and create currentVideo Input

--- src/app/video-details/video-details.component.ts
+++ src/app/video-details/video-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Video } from './../entities/video';
 
 @Component({
   moduleId: module.id,
@@ -7,5 +9,5 @@ import { Component } from '@angular/core';
 })
 
 export class VideoDetailsComponent {
-
+  @Input() currentVideo: Video;
 }
--- src/app/video-details/video-details.module.ts
+++ src/app/video-details/video-details.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { VideoDetailsComponent } from './video-details.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     VideoDetailsComponent
+  ],
+  imports: [
+    SharedModule
   ],
   exports: [
     VideoDetailsComponent
