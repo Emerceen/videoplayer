9746ab0a5cfe145f5d50e71fbda51fa78871e140
marcin.dedio <marcindedio@gmail.com>
2017-05-09 12:07:48 +0200

    Implementation ChannelDataService in Communication class

--- src/app/data-services/communication.ts
+++ src/app/data-services/communication.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+
 import { VideoDataService } from './video.service';
+import { ChannelDataService } from './channel.service';
 
 @Injectable()
 
 
 export class Communication {
   constructor(
-    public videoDataService: VideoDataService
+    public videoDataService: VideoDataService,
+    public channelDataService: ChannelDataService
   ) {}
 }
